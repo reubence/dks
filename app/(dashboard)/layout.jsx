9c972021c4ba72381acdb6e304d8f0085a106a7b
Reuben Rapose <reuben.rapose@gmail.com>
2023-08-08 11:20:23 +0530

    Added Route Handling

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -5,25 +5,41 @@ import Image from "next/image";
 import HOME from "@/public/images/icons/home-icon.svg";
 import LEADERBOARD from "@/public/images/icons/leaderboard-icon.svg";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import NavigationMenu from "@/components/navigation-menu";
 import LOGO_MAIN from "@/public/images/LOGO_MAIN.png";
 import INTERLACE_LOGO from "@/public/images/interlace-logo.svg";
 import WEB from "@/public/images/icons/web.svg";
 import TWITTER from "@/public/images/icons/twitter-icon.svg";
 import DISCORD from "@/public/images/icons/discord-icon.svg";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const DashboardLayout = ({ children }) => {
-  const [active, setActive] = React.useState("dashboard");
+  const pathname = usePathname();
+  console.log(pathname);
+  const [active, setActive] = React.useState(
+    pathname === "/dashboard" ? "dashboard" : "leaderboard"
+  );
+
+  React.useEffect(() => {
+    if (pathname === "/dashboard") {
+      setActive("dashboard");
+    } else if (pathname === "/leaderboard") {
+      setActive("leaderboard");
+    }
+  }, [pathname]);
+
   return (
     <>
       <NavigationMenu isLoggedIn={true} />
       <div className="w-fit mx-auto lg:w-full h-full flex">
-        <div className="hidden h-full w-72 border-r border-r-border lg:flex lg:flex-col justify-between">
+        <div className="hidden h-full w-72 border-r border-r-border lg:flex lg:flex-col lg:flex-shrink-0 justify-between">
           <div className="flex flex-col h-full pt-14">
-            <Button
-              variant="ghost"
+            <Link
+              href="/dashboard"
               className={cn(
+                buttonVariants({ variant: "ghost" }),
                 `justify-normal capitalize my-2 py-2 px-6 !text-3xl font-normal text-primary-foreground/40 font-wavesTiny`,
                 {
                   "text-primary-foreground border-r border-r-accent":
@@ -33,11 +49,12 @@ const DashboardLayout = ({ children }) => {
             >
               <Image src={HOME} alt="DKS Logo" className="w-6 h-6 mr-3" />
               <span className="mb-1.5">Dashboard</span>
-            </Button>
+            </Link>
 
-            <Button
-              variant="ghost"
+            <Link
+              href="/leaderboard"
               className={cn(
+                buttonVariants({ variant: "ghost" }),
                 `justify-normal capitalize my-2 py-2 px-6 !text-3xl font-normal text-primary-foreground/40 font-wavesTiny`,
                 {
                   "text-primary-foreground border-r border-r-accent":
@@ -51,7 +68,7 @@ const DashboardLayout = ({ children }) => {
                 className="w-6 h-6 mr-3"
               />
               <span className="mb-1.5">Leaderboard</span>
-            </Button>
+            </Link>
           </div>
           <div className="flex flex-col h-full w-full items-center justify-end py-28">
             <Image
