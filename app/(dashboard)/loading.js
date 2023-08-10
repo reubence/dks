f2bc78b03309abe84e13e1a5a9bce2100b392c81
Reuben Rapose <reuben.rapose@gmail.com>
2023-08-10 13:18:24 +0530

    Test Loading

--- app/(dashboard)/loading.js
+++ app/(dashboard)/loading.js
@@ -1,15 +1,15 @@
 import React from "react";
 import LOGO_MAIN from "@/public/images/LOGO_MAIN.png";
+import SKULL_SMALL from "@/public/images/icons/skull-logo-small.svg";
 import Image from "next/image";
 
 function Loading() {
   return (
     <div className="w-full h-full flex justify-center items-center align-middle">
       <Image
-        src={LOGO_MAIN}
+        src={SKULL_SMALL}
         alt="DKS Logo"
         className="-mt-20 w-[7vw] h-[7vw] animate-pulse absolute"
-        unoptimized
       />
     </div>
   );
