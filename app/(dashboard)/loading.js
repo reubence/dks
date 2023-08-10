a3b9bed45d549d707d0f048eba71f73e6f26b1ee
Reuben Rapose <reuben.rapose@gmail.com>
2023-08-10 13:32:20 +0530

    Testing

--- app/(dashboard)/loading.js
+++ app/(dashboard)/loading.js
@@ -6,7 +6,11 @@ import Image from "next/image";
 function Loading() {
   return (
     <div className="w-full h-full flex justify-center items-center align-middle">
-      Loading...{" "}
+      <Image
+        src={LOGO_MAIN}
+        alt="DKS Logo"
+        className="-mt-20 w-[7vw] h-[7vw] animate-pulse absolute"
+      />
     </div>
   );
 }
