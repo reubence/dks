475043b648135ed803a45d343d79d5afb8514461
Reuben Rapose <reuben.rapose@gmail.com>
2023-08-11 01:05:10 +0530

    Loading

--- app/(dashboard)/loading.jsx
+++ app/(dashboard)/loading.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import LOGO_MAIN from "@/public/images/LOGO_MAIN.svg";
+import LOGO_MAIN from "@/public/images/LOGO_MAIN.png";
 import SKULL_SMALL from "@/public/images/icons/skull-logo-small.svg";
 import Image from "next/image";
 
 function Loading() {
   return (
-    <div className="w-full h-full flex justify-center items-center align-middle">
+    <div className="w-full h-full flex flex-col justify-center items-center align-middle animate-pulse">
       <Image
         src={LOGO_MAIN}
         alt="DKS Logo"
-        className="-mt-20 w-[7vw] h-[7vw] animate-pulse absolute"
+        className="-mt-20 mb-2.5 w-[7vw] h-[7vw] "
       />
+      Loading...
     </div>
   );
 }
